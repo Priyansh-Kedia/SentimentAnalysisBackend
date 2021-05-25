cd791409b23fc9ff9f66cb80a3d8bae3fc0ca894
Aditya Sharma <aditya.sh2599@gmail.com>
2021-05-25 21:37:41 +0530

    test_endpoint

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # FastApi will be used to make the apis, 
 # refer to https://fastapi.tiangolo.com/ to read about the same
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def test_endpoint(review: str, is_positive: int,check_prediction: bool ):
+    print("Review :",review,"\n")
+    print("Review :",is_positive,"\n")
+    print("Review :",check_prediction,"\n")
+    return 1
