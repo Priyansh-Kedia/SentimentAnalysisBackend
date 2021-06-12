2aa2c0bfe8caa5c6fca7858aab57d03b7a0089a7
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-06-12 20:32:59 +0530

    added template

---
+++ ContactUs/js/main.js
@@ -0,0 +1,79 @@
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					// name: "required",
+					// subject: "required",
+					// email: {
+					// 	required: true,
+					// 	email: true
+					// },
+					// message: {
+					// 	required: true,
+					// 	minlength: 5
+					// }
+					review: "required"
+				},
+				messages: {
+					// name: "Please enter your name",
+					// subject: "Please enter your subject",
+					// email: "Please enter a valid email address",
+					// message: "Please enter a message"
+					review: "Please enter a review"
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+					let url = "/get_review/" + document.getElementById("review").value
+					console.log(url)
+					$.ajax({   	
+				      type: "GET",
+				      url: url,
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+						   $('#form-message-success').text(msg)
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+						
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+				      },
+				      error: function(e) {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} 
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+from os import name
 import string
 from sentiment_analysis.constants import STORAGE_BUCKET
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Request
 from utils import *
 
 from sentiment_analysis.models import *
 from sentiment_analysis.sentiment_analysis import get_sentiment, retrain_model
 
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+
 app = FastAPI()
+app.mount("/css", StaticFiles(directory="ContactUs/css"), name="static")
+app.mount("/js", StaticFiles(directory="ContactUs/js"), name="js")
+templates = Jinja2Templates(directory="ContactUs")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_prediction_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request":request})
 
 @app.post("/add_review/")
 async def add_review(review: Review = Body(...)):
